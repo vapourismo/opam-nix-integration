03693a0749ee6c988def6f32133776b4e43b1ec9
Ole Krüger <ole@vprsm.de>
2022-02-27 13:30:12 +0000

    Allow opam2nix to be built as a package

--- flake.nix
+++ flake.nix
@@ -4,15 +4,36 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs;
     flake-utils.url = github:numtide/flake-utils;
+    flake-compat = {
+      url = github:edolstra/flake-compat;
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, flake-compat }: flake-utils.lib.eachDefaultSystem (system:
     with import nixpkgs { inherit system; };
 
     let
       ocamlPackages = ocaml-ng.ocamlPackages_4_13;
     in
     {
+      defaultPackage = ocamlPackages.buildDunePackage {
+        pname = "opam2nix";
+        version = "0.0.0";
+
+        useDune2 = true;
+
+        src = self;
+
+        propagatedBuildInputs = with ocamlPackages; [
+          opam-format
+          ppxlib
+          ppx_deriving
+          cmdliner
+          zarith
+        ];
+      };
+
       devShell = mkShell {
         nativeBuildInputs = with ocamlPackages; [
           ocaml
@@ -23,12 +44,7 @@
         buildInputs = with ocamlPackages; [
           ocaml-lsp
           ocamlformat
-          opam-format
-          ppxlib
-          ppx_deriving
-          cmdliner
-          zarith
-        ];
+        ] ++ self.defaultPackage.${system}.propagatedBuildInputs;
       };
     }
   );
