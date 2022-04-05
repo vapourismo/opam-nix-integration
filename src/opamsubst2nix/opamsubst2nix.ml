d8fbac6a5cdfa1bb217cb4a049e3b09e73ce49d8
Ole Krüger <ole@vprsm.de>
2022-04-05 21:01:49 +0100

    Simplify variable parser

--- src/opamsubst2nix/opamsubst2nix.ml
+++ src/opamsubst2nix/opamsubst2nix.ml
@@ -8,21 +8,37 @@ let read_one_line () =
   | End_of_file -> None
 ;;
 
-let lines =
+let input =
   Seq.unfold (fun () -> Option.map (fun line -> line, ()) (read_one_line ())) ()
   |> List.of_seq
   |> String.concat "\n"
 ;;
 
-let () =
-  let expr =
-    Result.fold ~ok:Fun.id ~error:failwith
-    @@ Angstrom.(
-         parse_string
-           ~consume:All
-           (Lib.interpolated_string_parser (Nix.ident "__argScope"))
-           lines)
+let scope = Nix.ident "__argScope"
+
+let segments =
+  let rec with_variable head tail =
+    match String.split_on_char '}' head with
+    | [ name; "" ] -> Nix.CodeSegment (Lib.nix_of_ident_string scope name) :: start tail
+    | _ -> failwith (Printf.sprintf "Bad variable interpolation: %s" head)
+  and after_percent = function
+    | [] -> []
+    | head :: tail ->
+      if String.starts_with ~prefix:"{" head
+      then (
+        let head = String.sub head 1 (String.length head - 1) in
+        with_variable head tail)
+      else Nix.StringSegment head :: after_percent tail
+  and start segments =
+    match segments with
+    | head :: tail -> Nix.StringSegment head :: after_percent tail
+    | [] -> []
   in
-  let lambda = Nix.(lambda (Pattern.ident "__argScope") expr) in
-  print_string (Nix.render lambda)
+  start (String.split_on_char '%' input)
+;;
+
+let () =
+  let open Nix in
+  let exp = lambda (Pattern.ident "__argScope") (MultilineString [ segments ]) in
+  print_endline (render exp)
 ;;
