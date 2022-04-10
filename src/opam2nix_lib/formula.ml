c95e47b95b222630018f8d4de23f75275b4770bf
Ole Krüger <ole@vprsm.de>
2022-04-10 16:20:18 +0100

    Use more of Nix.scoped

--- src/opam2nix_lib/formula.ml
+++ src/opam2nix_lib/formula.ml
@@ -1,21 +1,18 @@
 let to_nix to_nix formula =
   let open Nix in
-  let scope = ident "__formulaScope" in
-  let rec go formula =
-    match formula with
-    | OpamFormula.Empty -> index scope "empty"
-    | Atom atom -> index scope "atom" @@ [ to_nix atom ]
-    | Block formula -> go formula
-    (* The formula is in CNF, these two cases should never happen because [go] is called on
+  scoped "__formulaScope" (fun scope ->
+      let rec go formula =
+        match formula with
+        | OpamFormula.Empty -> index scope "empty"
+        | Atom atom -> index scope "atom" @@ [ to_nix atom ]
+        | Block formula -> go formula
+        (* The formula is in CNF, these two cases should never happen because [go] is called on
        "atom"-like items of the formula. *)
-    | And _ -> failwith "CNF conversion failed!"
-    | Or _ -> failwith "CNF conversion failed!"
-  in
-  let body =
-    OpamFormula.cnf_of_formula formula
-    |> OpamFormula.ands_to_list
-    |> List.map (fun ors -> OpamFormula.ors_to_list ors |> List.map go |> list)
-    |> list
-  in
-  lambda (Pattern.ident "__formulaScope") body
+        | And _ -> failwith "CNF conversion failed!"
+        | Or _ -> failwith "CNF conversion failed!"
+      in
+      OpamFormula.cnf_of_formula formula
+      |> OpamFormula.ands_to_list
+      |> List.map (fun ors -> OpamFormula.ors_to_list ors |> List.map go |> list)
+      |> list)
 ;;
