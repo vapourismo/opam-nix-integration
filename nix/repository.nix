d2ce4bef29fac4fd76623525cd831b5c748f7932
Ole Krüger <ole@vprsm.de>
2022-04-14 19:08:16 +0100

    Upgrade OPAM repository

--- nix/repository.nix
+++ nix/repository.nix
@@ -3,6 +3,6 @@
 fetchFromGitHub {
   owner = "ocaml";
   repo = "opam-repository";
-  rev = "4fbcb30f190d2e8d04cc65dece65cb4d0ee539fa";
-  sha256 = "sha256-yvOVhaBPTJoIsG4W5kXhxa2hccNKrte2QQgMfpHni5I=";
+  rev = "f8b03f4de2f78ae4127b0314ba818730e882a770";
+  sha256 = "sha256-xKq9DgbE+m5XPug+f2ZWH/V3wq0wDTPcqn814HMQ8Ss=";
 }
