ac0ba28e3ea6707a58ab71f9dcbeefb20de77646
Ole Krüger <ole@vprsm.de>
2022-04-15 23:46:13 +0100

    Split up filter lib

---
+++ nix/lib/opam/filter/eval.nix
@@ -0,0 +1,70 @@
+{ envLib }:
+
+let
+  scope =
+    let self = { allowMissing ? false }: {
+      # bool -> bool
+      bool = value: value;
+
+      # string -> string
+      string = value: value;
+
+      # env -> any
+      ident = envLib.eval {
+        onMissing =
+          if allowMissing then
+            _: _: null
+          else
+            envLib.defaultOnMissing;
+      };
+
+      # any -> any -> bool
+      equal = lhs: rhs:
+        if lhs == null || rhs == null then null else lhs == rhs;
+
+      # any -> any -> bool
+      notEqual = lhs: rhs:
+        if lhs == null || rhs == null then null else lhs != rhs;
+
+      # any -> any -> bool
+      greaterEqual = lhs: rhs:
+        if lhs == null || rhs == null then null else lhs >= rhs;
+
+      # any -> any -> bool
+      greaterThan = lhs: rhs:
+        if lhs == null || rhs == null then null else lhs > rhs;
+
+      # any -> any -> bool
+      lowerEqual = lhs: rhs:
+        if lhs == null || rhs == null then null else lhs <= rhs;
+
+      # any -> any -> bool
+      lowerThan = lhs: rhs:
+        if lhs == null || rhs == null then null else lhs < rhs;
+
+      # bool -> bool -> bool
+      and = lhs: rhs:
+        if (lhs == null && rhs == false) || (rhs == null && lhs == false) then
+          false
+        else
+          lhs && rhs;
+
+      # bool -> bool -> bool
+      or = lhs: rhs:
+        if (lhs == null && rhs == true) || (rhs == null && lhs == true) then
+          true
+        else
+          lhs || rhs;
+
+      # bool -> bool
+      not = x: !x;
+
+      # filter -> bool
+      def = filter: filter (self { allowMissing = true; }) != null;
+
+      # ?
+      undef = abort "Bad use of filter.undef!";
+    }; in self;
+in
+
+filter: filter (scope { })
---
+++ nix/lib/opam/filter/show.nix
@@ -0,0 +1,47 @@
+{ lib, envLib }:
+
+let
+  scope = {
+    bool = builtins.toJSON;
+
+    string = builtins.toJSON;
+
+    ident = f:
+      let value = envLib.eval { } f; in
+      f {
+        local = { name, ... }: "${value} (= ${name})";
+
+        package = { packageName, name, ... }: "${value} (= ${packageName}:${name})";
+
+        combine = values:
+          lib.foldl'
+            (lhs: rhs: "${lhs} & ${rhs}")
+            (lib.head values)
+            (lib.tail values);
+      };
+
+    equal = lhs: rhs: "${lhs} == ${rhs}";
+
+    notEqual = lhs: rhs: "${lhs} != ${rhs}";
+
+    greaterEqual = lhs: rhs: "${lhs} >= ${rhs}";
+
+    greaterThan = lhs: rhs: "${lhs} > ${rhs}";
+
+    lowerEqual = lhs: rhs: "${lhs} <= ${rhs}";
+
+    lowerThan = lhs: rhs: "${lhs} < ${rhs}";
+
+    and = lhs: rhs: "${lhs} && ${rhs}";
+
+    or = lhs: rhs: "${lhs} || ${rhs}";
+
+    def = filter: "?(${show filter})";
+
+    undef = abort "filter.undef";
+  };
+
+  show = filter: filter scope;
+in
+
+show
