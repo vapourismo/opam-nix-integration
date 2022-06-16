2f39cfbbca34bac1dfa4072024332e8230e977ee
Ole Krüger <ole@vprsm.de>
2022-06-16 11:39:26 +0100

    Treat 'base' versions differently

--- nix/lib/opam/constraint/eval.nix
+++ nix/lib/opam/constraint/eval.nix
@@ -1,7 +1,17 @@
 { lib, filterLib, cleanVersion }:
 
 let
-  compareVersions = lhs: rhs: lib.strings.compareVersions (cleanVersion lhs) (cleanVersion rhs);
+  compareVersions = lhs: rhs:
+    # We treat 'base' in a special way. It indicates that a package came with
+    # the compiler.
+    if lhs == "base" && rhs == "base" then
+      0
+    else if lhs == "base" then
+      1
+    else if rhs == "base" then
+      -1
+    else
+      lib.strings.compareVersions (cleanVersion lhs) (cleanVersion rhs);
 in
 
 constraint: version: constraint {
