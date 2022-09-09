0379db3093e5f6e353982cf63b32689cd6dd1e0d
Ole Krüger <ole@vprsm.de>
2022-09-09 23:22:48 +0100

    Move generateOpam2Nix implementation to file

--- nix/scope/opam/default.nix
+++ nix/scope/opam/default.nix
@@ -20,27 +20,7 @@ lib.makeScope pkgs.newScope (self: {
 
   selectOpamSrc = src: altSrc: if altSrc != null then altSrc else src;
 
-  generateOpam2Nix = { name, version, opam, patches ? [ ], extraFiles ? null }:
-    import (
-      runCommand
-        "opam2nix-${name}-${version}"
-        {
-          buildInputs = [ opam2nix ];
-          inherit opam patches;
-        }
-        ''
-          cp $opam opam
-          chmod +w opam
-          for patch in $patches; do
-            patch opam $patch
-          done
-          opam2nix \
-            --name ${name} \
-            --version ${version} \
-            ${lib.optionalString (extraFiles != null && lib.pathExists extraFiles) "--extra-files ${extraFiles}"} \
-            --file opam > $out
-        ''
-    );
+  generateOpam2Nix = callPackage ./generate-opam2nix.nix { };
 
   callOpam2Nix = { name, opam ? null, src ? null, ... }@args: extra:
     let
---
+++ nix/scope/opam/generate-opam2nix.nix
@@ -0,0 +1,24 @@
+{ lib, runCommand, opam2nix }:
+
+{ name, version, opam, patches ? [ ], extraFiles ? null }:
+
+import (
+  runCommand
+    "opam2nix-${name}-${version}"
+  {
+    buildInputs = [ opam2nix ];
+    inherit opam patches;
+  }
+    ''
+      cp $opam opam
+      chmod +w opam
+      for patch in $patches; do
+        patch opam $patch
+      done
+      opam2nix \
+        --name ${name} \
+        --version ${version} \
+        ${lib.optionalString (extraFiles != null && lib.pathExists extraFiles) "--extra-files ${extraFiles}"} \
+        --file opam > $out
+    ''
+)
