cdce73f790d447a5a5648196877380b71fd0b69f
Ole Krüger <ole@vprsm.de>
2022-09-10 21:25:00 +0100

    Decouple generateOpam2Nix utility function

--- nix/scope/opam/default.nix
+++ nix/scope/opam/default.nix
@@ -9,7 +9,30 @@
 , opam2nix
 , opamvars2nix
 , opamsubst2nix
-}@args:
+}:
+
+let
+  mkCallOpam2Nix = { callPackage, opam2nix }:
+    let
+      generateOpam2Nix = callPackage ./generate-opam2nix.nix {
+        inherit opam2nix;
+      };
+    in
+
+    { name, opam ? null, src ? null, ... }@args: extra:
+      let
+        argOverride =
+          if opam != null then
+            { }
+          else if src != null then
+            { opam = "${src}/${name}.opam"; }
+          else
+            abort "'opam' mustn't be null if 'src' is also null!";
+      in
+      callPackage
+        (generateOpam2Nix (builtins.removeAttrs args [ "src" ] // argOverride))
+        ({ altSrc = src; } // extra);
+in
 
 lib.makeScope pkgs.newScope (final: {
   mkOpamDerivation = final.callPackage ./make-opam-derivation.nix {
@@ -18,21 +41,7 @@ lib.makeScope pkgs.newScope (final: {
 
   selectOpamSrc = src: altSrc: if altSrc != null then altSrc else src;
 
-  generateOpam2Nix = final.callPackage ./generate-opam2nix.nix {
+  callOpam2Nix = final.callPackage mkCallOpam2Nix {
     inherit opam2nix;
   };
-
-  callOpam2Nix = { name, opam ? null, src ? null, ... }@args: extra:
-    let
-      argOverride =
-        if opam != null then
-          { }
-        else if src != null then
-          { opam = "${src}/${name}.opam"; }
-        else
-          abort "'opam' mustn't be null if 'src' is also null!";
-    in
-    final.callPackage
-      (final.generateOpam2Nix (builtins.removeAttrs args [ "src" ] // argOverride))
-      ({ altSrc = src; } // extra);
 })
